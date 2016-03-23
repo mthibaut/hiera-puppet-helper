350e4c8de2b17c051f4c001d0a6068fd36414a7b
Chris Brinker <chris.brinker@gmail.com>
2016-03-22 17:21:48 -0700

    In case the answer is the default argument, do not error, return the answer

--- lib/hiera-puppet-helper/puppet.rb
+++ lib/hiera-puppet-helper/puppet.rb
@@ -48,7 +48,7 @@ class Puppet::Parser::Compiler
 
       answer = hiera.lookup(key, default, hiera_scope, override, :priority)
 
-      raise(Puppet::ParseError, "Could not find data item #{key} in any Hiera data file and no default supplied") if answer.nil?
+      raise(Puppet::ParseError, "Could not find data item #{key} in any Hiera data file and no default supplied") if answer.nil? and answer != default
 
       return answer
     end
@@ -77,7 +77,7 @@ class Puppet::Parser::Compiler
 
       answer = hiera.lookup(key, default, hiera_scope, override, :array)
 
-      raise(Puppet::ParseError, "Could not find data item #{key} in any Hiera data file and no default supplied") if answer.nil?
+      raise(Puppet::ParseError, "Could not find data item #{key} in any Hiera data file and no default supplied") if answer.nil? and answer != default
 
       answer
     end
@@ -108,7 +108,7 @@ class Puppet::Parser::Compiler
 
       answer = hiera.lookup(key, default, hiera_scope, override, :hash)
 
-      raise(Puppet::ParseError, "Could not find data item #{key} in any Hiera data file and no default supplied") if answer.nil?
+      raise(Puppet::ParseError, "Could not find data item #{key} in any Hiera data file and no default supplied") if answer.nil? and answer != default
 
       answer
     end
@@ -137,7 +137,7 @@ class Puppet::Parser::Compiler
 
       answer = hiera.lookup(key, default, hiera_scope, override, :array)
 
-      raise(Puppet::ParseError, "Could not find data item #{key} in any Hiera data file and no default supplied") if answer.nil?
+      raise(Puppet::ParseError, "Could not find data item #{key} in any Hiera data file and no default supplied") if answer.nil? and answer != default
 
       method = Puppet::Parser::Functions.function(:include)
       send(method, answer)
